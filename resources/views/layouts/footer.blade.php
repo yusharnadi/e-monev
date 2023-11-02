a7fb544049e1330be5c87b1ab4de7611c21dfe13
yusharnadi <yust.44@gmail.com>
2023-11-02 22:35:56 +0700

    add layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; 2022 <div class="bullet"></div> {{config('app.name')}} <a href="{{route('dashboard')}}" target="_blank"> | BAGIAN ORGANISASI SETDA KOTA SINGKAWANG</a>
+    </div>
+    <div class="footer-right">
+      V 1.0
+    </div>
+</footer>
