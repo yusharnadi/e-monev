8fa08e351632f7c01f90317a799fccf6d400e255
yusharnadi <yust.44@gmail.com>
2023-11-17 23:18:17 +0700

    add created by

--- database/migrations/2023_11_07_153155_create_pdam_reports_table.php
+++ database/migrations/2023_11_07_153155_create_pdam_reports_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('year');
             $table->string('filename');
             $table->text('note');
+            $table->integer('created_by');
             $table->timestamps();
         });
     }
