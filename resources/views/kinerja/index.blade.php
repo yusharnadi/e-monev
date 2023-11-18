510415b93672263ce0a0c3eb5b4f4170112b19b5
yusharnadi <yust.44@gmail.com>
2023-11-18 20:56:57 +0700

    adding modal delete

--- resources/views/kinerja/index.blade.php
+++ resources/views/kinerja/index.blade.php
@@ -40,7 +40,7 @@
                                     <a href="{{route('kinerja.edit', $kinerja['id'])}}" class="btn btn-icon btn-sm btn-danger"><i class="fas fa-file-pdf"></i></a>
                                     <a href="{{route('kinerja.show', $kinerja['id'])}}" class="btn btn-icon btn-sm btn-primary"><i class="fas fa-eye"></i></a>
                                     <a href="{{route('kinerja.edit', $kinerja['id'])}}" class="btn btn-icon btn-sm btn-warning"><i class="fas fa-edit"></i></a>
-                                    <a href="{{route('kinerja.edit', $kinerja['id'])}}" class="btn btn-icon btn-sm btn-danger"><i class="fas fa-trash"></i></a>
+                                    <a data-toggles="modal" id="smallButton" data-target="#deleteModal" data-action="{{route('kinerja.destroy', $kinerja->id)}}" class="btn btn-icon btn-sm btn-danger btn-fire" href="#"><i class="fas fa-trash"></i></a>
                                 </td>
                             </tr>
                         @endforeach
@@ -51,6 +51,31 @@
         </div>
     </div>
 @endsection
+@push('modal')
+  <div class="modal fade" tabindex="-1" role="dialog" id="deleteModal" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <form action="" method="POST" id="formDelete">
+          @csrf
+          @method('DELETE')
+        <div class="modal-header">
+          <h5 class="modal-title">Konfirmasi Hapus Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Apakah Anda yakin menghapus data ? </p>
+        </div>
+        <div class="modal-footer bg-whitesmoke br">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-danger">Save changes</button>
+        </div>
+      </form>
+      </div>
+    </div>
+  </div>
+@endpush
 @push('css-datatables')
     <link rel="stylesheet" href="{{asset('assets/modules/DataTables/dt/css/dataTables.bootstrap4.min.css')}}">
 @endpush
@@ -60,6 +85,13 @@
     <script>
         $(document).ready( function () {
             $('.datatable').DataTable();
+            $('.btn-fire').click(function(){
+            $('#deleteModal').modal({ backdrop: 'static', keyboard: false })
+            $('#formDelete').attr('action', $(this).data('action'))})
+
+            $('#deleteModal').on('hidden.bs.modal', function (e) {
+                $('#formDelete').attr('action', '')
+            })
         });
     </script>
 @endpush
