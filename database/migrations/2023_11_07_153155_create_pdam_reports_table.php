5e4b82b89fc968079577ef9130c3fbe15ccb06ff
yusharnadi <yust.44@gmail.com>
2023-11-21 02:06:50 +0700

    add nullable on note

--- database/migrations/2023_11_07_153155_create_pdam_reports_table.php
+++ database/migrations/2023_11_07_153155_create_pdam_reports_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('month');
             $table->string('year');
             $table->string('filename');
-            $table->text('note');
+            $table->text('note')->nullable();
             $table->timestamps();
         });
     }
